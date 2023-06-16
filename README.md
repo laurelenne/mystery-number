30a8a7c8f12cff70cc192a5ad792ba4f4b90283b
Laurée <poussin.laurelenne@gmail.com>
2023-06-17 00:53:40 +0200

    New version

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # https://laurelenne.github.io/mystery-number
+
+Nous avons généré un nombre aléatoire entre <b>1 et 100</b>.<br>
+Tentez de le trouver en <b>10 tours maximum</b>. <br>
+Pour chaque tentative, nous vous indiquerons si le nombre mystère<br> est plus grand ou plus petit.<br>
+A vous de jouer !
+
+Use :
+- Bootstrap
+- Jquery
--- js/app.js
+++ js/app.js
@@ -1,27 +1,13 @@
-//Généré un nombre aléatoire entre 0 et 100
-let intRandom = Math.floor(Math.random() * 100) + 1;
-console.log(intRandom);
-let input = document.getElementById('input');
-const button = document.getElementById("bouton");
-// Tableau vide des essais
-let tabNumber = [];
+// Généré un nombre aléatoire entre 0 et 100
+var intRandom = Math.floor(Math.random() * 100) + 1;
+// DOM elements
+var progress = (".progress-bar").html;
 
-let compteur = 1;
+var btnValid = ("#btn-valid").html;
+var info = ('#info').html
+var trys = [];
+var compteur = 1;
 
-let b = document.body;
-
-let p1 = document.getElementById('p1');
-let p2 = document.getElementById('p2');
-let p3 = document.getElementById('p3');
-
-let newP1 = document.createElement('p',{class:"col text-light"});
-let newP2 = document.createElement('p',{class:"col text-light"});
-let newP3 = document.createElement('p', {class:"col text-light"});
-
-//Ajoute un paragraphe après p1
-p1.insertAdjacentElement('beforeend', newP1);
-p2.insertAdjacentElement('beforeend', newP2);
-p3.insertAdjacentElement('beforeend', newP3);
 
 function modifElement(chiffreGrandPetit, tabNumber, erreurInput) {
     newP1.textContent = chiffreGrandPetit;
@@ -29,29 +15,35 @@ function modifElement(chiffreGrandPetit, tabNumber, erreurInput) {
     newP3.textContent = erreurInput;
 }
 
-// Verifs Input
-const verif = () => {
+
+
+
+// Check Input
+const checkValue = () => {
+var input = ("#input").val();
+console.log(input);
+    // Si la valeur n'est pas OK
+        // Si la valeur n'est pas un nombre
+            // Alors
+        // Sinon si le nombre n'est pas comprise entre 0 et 100
+    // Sinon si la valeur est OK
+
     // Input Value (String)
-    let inputValue = document.getElementById('input').value;
-    let typeOfinputValue = typeof (inputValue);
-    let inputValueInt = parseInt(inputValue);
+    var inputValue = document.getElementById('input').value;
+    var typeOfinputValue = typeof(inputValue);
+    var inputValueInt = parseInt(inputValue);
 
     if (inputValue.length == 0) {
-        console.log('Choisir un nombre entre 1 et 99');
         modifElement('Choisir un nombre entre 1 et 99', "", tabNumber);
         input.style.borderColor = "red";
     } else if (isNaN(inputValueInt) == true) {
-        console.log("Réessayer ce n'est pas un nombre");
         modifElement("Réessayer ce n'est pas un nombre", "", tabNumber);
         input.style.borderColor = "red";
     } else {
-        console.log("C'est bien un nombre");
         if (inputValue.length > 2) {
             modifElement("Deux chiffre suffise !");
-            console.log('Deux chiffre suffise !', "", tabNumber)
             input.style.borderColor = "red";
         } else {
-            console.clear();
             verif1(inputValueInt);
         }
     }
@@ -61,31 +53,28 @@ const verif = () => {
 const verif1 = (inputValueInt) => {
     //S'il est correct
     if (inputValueInt === intRandom) {
-        console.log("Bravo");
         input.style.borderColor = "green";
         modifElement("Bravo !", "", tabNumber);
     }
     //S'il est faux et que le joueur a encore des chances
     else if ((inputValueInt != intRandom) && (compteur < 10)) {
         helpMessage(inputValueInt, intRandom);
-        let chances = 10 - compteur;
-        console.log('Essaye encore tu as encore ' + chances + " chances");
+        var chances = 10 - compteur;
         compteur++;
         tabNumber.push(inputValueInt);
-        console.log("Tu as déjà essayer : " + tabNumber.toString());
         modifElement('Essaye encore tu as encore ' + chances + " chances", helpMessage(inputValueInt, intRandom), tabNumber);
     }
     //S'il est faux et que le joueur n'a plus de tour à jouer
     else if ((inputValueInt != intRandom) && (compteur >= 10)) {
         helpMessage(inputValueInt, intRandom);
-        console.log("Tu n'a plus de chance !");
-        console.log('FIN');
     }
 }
 
 // Affiche si le chiffre a trouvé est plus grand ou plus petit
 const helpMessage = (inputValueInt, intRandom) => {
-    let message = "";
+    var message = "";
     inputValueInt < intRandom ? message = 'Le chiffre à trouver est plus grand' : message = 'Le chiffre à trouver est plus petit';
     return message;
 };
+
+btnValid.
